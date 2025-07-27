4eb64e463f80358a1147df757fb3afb4eb2604fd
Михаил Солнцев <mihsoln@live.com>
2025-07-27 21:23:54 +0300

    Выполнение блока 3 курса WPF

--- TaskWPF3_1_StudyManager/MainWindow.xaml.cs
+++ TaskWPF3_1_StudyManager/MainWindow.xaml.cs
@@ -21,12 +21,71 @@ namespace TaskWPF3_1_StudyManager
             InitializeComponent();
         }
 
-        private void refreshCourses_Click(object sender, RoutedEventArgs e)
+        private void EduHours_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            hours.Text = EduHours.Value.ToString();
+        }
+
+        private void next_Click(object sender, RoutedEventArgs e)
+        {
+            //Проверка заполнения ФИО студента
+            if (studentName.Text == null || studentName.Text == "")
+            {
+                MessageBox.Show("Не указано ФИО студента.", "Ошибка",
+                              MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            string name = studentName.Text;
+
+            //Проверка выбора факультета
+            if (faculty.SelectedItem == null) 
+            {
+                MessageBox.Show("Не выбран факультет.", "Ошибка",
+                              MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            string? facultyName = faculty.Text;
+
+            //Проверка выбора курсов
+            if (courses.SelectedItems.Count < 1)
+            {
+                MessageBox.Show("Не выбраны желаемые курсы.", "Ошибка",
+                              MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            string coursesToLearn = "";
+            foreach (string courseName in courses.SelectedItems.Cast<string>().ToList())
+            {
+                if (coursesToLearn == "") coursesToLearn += courseName;
+                else coursesToLearn += ", " + courseName;
+            }
+            List<string> coursesList = courses.SelectedItems.Cast<string>().ToList();
+
+            //Проверка наличия согласия
+            if (!(bool)approval.IsChecked)
+            {
+                MessageBox.Show("Необходимо дать согласие на обработку данных", "Ошибка",
+                              MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            string educationForm = (bool)fullTime.IsChecked ? "Очная" : "Заочная";
+            int hours = (int)EduHours.Value;
+
+            MessageBox.Show($"Студент: {name}\nФакультет: {facultyName}\nКурсы: {coursesToLearn}\nФорма обучения: {educationForm}\nЧисло часов в неделю: {hours}", "Данные отправлены",
+                              MessageBoxButton.OK, MessageBoxImage.Information);
+            Environment.Exit(1);
+
+        }
+
+        private void faculty_MouseLeave(object sender, MouseEventArgs e)
         {
             if (faculty.SelectedItem == null) return;
             courses.Items.Clear();
 
-            switch (faculty.SelectedItem.ToString())
+            switch (faculty.Text)
             {
                 case "Программирование":
                     courses.Items.Add("Программирование C#");
