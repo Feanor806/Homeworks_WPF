3d1e5144da6e21901701e1da8cc367def02f97a6
mihso <solntsevma@pik.ru>
2025-07-27 14:59:44 +0300

    работа с блоком 3

---
+++ TaskWPF3_1_StudyManager/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TaskWPF3_1_StudyManager
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ TaskWPF3_1_StudyManager/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskWPF3_1_StudyManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void refreshCourses_Click(object sender, RoutedEventArgs e)
+        {
+            if (faculty.SelectedItem == null) return;
+            courses.Items.Clear();
+
+            switch (faculty.SelectedItem.ToString())
+            {
+                case "Программирование":
+                    courses.Items.Add("Программирование C#");
+                    courses.Items.Add("Программирование Python");
+                    courses.Items.Add("Программирование 1C");
+                    break;
+                case "Строительство":
+                    courses.Items.Add("Архитектурные решения");
+                    courses.Items.Add("Проектирование конструктивных элементов");
+                    courses.Items.Add("Генплан и ландшафтный дизайн");
+                    break;
+                case "Теплогазоснабжение и вентиляция":
+                    courses.Items.Add("Проектирование систем отопления");
+                    courses.Items.Add("Противодымная вентиляция зданий и сооружений");
+                    courses.Items.Add("Кондиционирование и микроклимат");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
